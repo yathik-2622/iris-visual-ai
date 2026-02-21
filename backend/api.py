7add31ec475251b62c84d1435f74ef09a9ef078f
yathik-2622 <134185643+yathik-2622@users.noreply.github.com>
2026-02-21 18:37:56 +0530

    🔒 Updated CORS with Vercel production URL

--- backend/api.py
+++ backend/api.py
@@ -12,7 +12,9 @@ app = FastAPI(title="Iris Prediction API")
 # Allow local frontend origins; adjust for production
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change to specific origins in production
+    allow_origins=["https://iris-visual-ai-frontend.vercel.app",
+                   "http://localhost:5500",
+                   "http://127.0.0.1:5500",],  # Change to specific origins in production
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
